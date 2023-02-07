f93aa67fe2d653ea74ca41812be460f3f538a5e6
davidscanz <infdsc02@gmail.com>
2023-02-07 19:51:50 +0100

    Se añade el WifiManager para poder añadir la configuración inicial. Este se ha modificado para, además de poder introducir los datos de la red WiFi, poder introducir los datos de configuración del broker MQTT al que se conectará.

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from time import sleep
 import network
 import utime
 import sys
+import wifimgr
 
-__MQTT_SERVER__ = <IP_TO_THE_MQTT_BROKER>
 
 __CLIENT_ID__ = ubinascii.hexlify(unique_id())
 __TOPIC_PUB__ = b'INTERCOM_RING'
 
 __ACTIVATION_THRESHOLD__ = 1000
 
-
-def mqtt_connect():
-    client = MQTTClient(__CLIENT_ID__, __MQTT_SERVER__)
+debounce_time = 0
+        
+        
+def mqtt_connect(server_host, port, user, password):
+    global __CLIENT_ID__
+    client = MQTTClient(client_id=__CLIENT_ID__, server=server_host, port=port, user=user, password=password)
     client.connect()
-    print('Connected to %s MQTT broker.' % (__MQTT_SERVER__))
+    print('Connected to %s MQTT broker.' % (server_host))
     return client
 
 
@@ -23,23 +26,35 @@ def restart_and_reconnect():
     print('Failed to connect to MQTT broker. Reconnecting...')
     sleep(10)
     reset()
-
-
+    
+        
 if __name__ == "__main__":
-    try:
-        client = mqtt_connect()
-    except OSError as e:
-        restart_and_reconnect()
-
     input_pin = ADC(Pin(33))
     input_pin.atten(ADC.ATTN_11DB)  # Full range: 3.3v
-
-    ADC.width(ADC.WIDTH_12BIT)
+    
     sample_count = 0
     acum = 0
     signal_act = False
+    
+    wlan = wifimgr.get_connection()
+    if wlan is None:
+        print("Could not initialize the network connection.")
+        while True:
+            pass  # you shall not pass :D
+        
+    try:
+        mqtt_broker_host, mqtt_broker_port, mqtt_broker_user, mqtt_broker_password = wifimgr.read_mqtt_broker_conf()
+        client = mqtt_connect(server_host=mqtt_broker_host, port=int(mqtt_broker_port), user=mqtt_broker_user, password=mqtt_broker_password)
+    except OSError as e:
+        print(str(e))
+        restart_and_reconnect()
 
+    
     while True:
+        
+        if gc.mem_free() < 102000:
+          gc.collect()
+          
         try:
             readed_value = input_pin.read()
             if sample_count < 10:
@@ -56,6 +71,7 @@ if __name__ == "__main__":
 
                 acum = 0
                 sample_count = 0
+            
             sleep(0.05)
         except OSError as e:
             restart_and_reconnect()
